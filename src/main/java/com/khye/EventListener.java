596aad27ead8e3ebaf6728df855229b8d87136f9
Khye Ean Low <lowkhyeean@gmail.com>
2020-05-10 12:18:07 +1000

    added onguildjoin and ready

--- src/main/java/com/khye/EventListener.java
+++ src/main/java/com/khye/EventListener.java
@@ -17,6 +17,8 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.guild.GenericGuildEvent;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -38,7 +40,17 @@ public class EventListener extends ListenerAdapter {
     }
 
     @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        checkAndCreateBot(event);
+    }
+
+    @Override
     public void onGuildReady(GuildReadyEvent event) {
+        // fired on start up
+        checkAndCreateBot(event);
+    }
+
+    private void checkAndCreateBot(GenericGuildEvent event) {
         String guildId = event.getGuild().getId();
         // Generate UUID
         UUID uuid = UUID.nameUUIDFromBytes(guildId.getBytes());
